d9da782f578fc19efda4b2ada81d9c3757b24ecf
NicholasMelendez <nicholasmelendez1221@gmail.com>
2026-06-12 15:53:51 -0500

    Updating main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef struct{ //struct to hold PGM data and specifications
+    double height;
+    double length;
+    char type[2];
+    double max_value;
+}Pgm;
+
+int main(void){
+    return 0;
+}
