34e730efabdd2a14fcd09152e805e0ce2a6b5745
NicholasMelendez <nicholasmelendez1221@gmail.com>
2026-06-18 12:41:00 -0500

    Updated main for image dimension processing

--- main.c
+++ main.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
 
-typedef struct{                        //struct to hold PGM data and specifications
-    double width;                      //side to side psixel count 
-    double height;                     //top to bottom pixel count 
-    double max_value;                  //max pixel value
-    unsigned char *image_array;        //array to store pgm data
+
+
+
+
+//struct to hold image width, height, max_value in pixels, array of data, and type. Correct type for binary pgm is "P5"
+
+typedef struct{                      
+    double width;                      
+    double height;                    
+    double max_value;                  
+    unsigned char *image_array;        
     char type[3];
 }Pgm;
 
 unsigned char* ReadPGM(char file_name[], Pgm*image);
 
-double height_calculation(void* pgm_array);
+double height_calculation(Pgm* image);
 
+
+//currently experimenting with test pgm
 int main(void){
     Pgm current_image;
     ReadPGM("test_pgm.pgm", &current_image);
-    printf("%lf %lf %lf", current_image.width, current_image.height, current_image.max_value);
-    
+    printf("%lf %lf %lf\n", current_image.width, current_image.height, current_image.max_value);
+    height_calculation(&current_image);
+
     return 0;
 }
 
+
+
+
+
+
+
+//ensures file type is correct, reads values from pgm to allocate enough memory, puts data into array
 unsigned char* ReadPGM(char filename[], Pgm *image){
     char c;
     char *type = (image->type); //poitner to type string
@@ -41,9 +59,14 @@ unsigned char* ReadPGM(char filename[], Pgm *image){
     }
 
     fscanf(current_pgm, "%lf %lf %lf",&(image->width), &(image->height), &(image->max_value));    //Obtain pgm width, height, and maximum value
-   
-    
+    image -> image_array = malloc((image-> height)* ( image -> width)*sizeof(char));
+    fread(image->image_array, sizeof(char), (image->height) * (image->width), current_pgm);
 
-
-
+}
+double height_calculation(Pgm *image){
+    int items = image->height * image->width;
+    printf("%d", items);
+    for(int i = 0; i<items; i++){
+        printf("%c\n", image ->image_array[i]);
+    }
 }
